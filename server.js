2b3e1e1249ddf1097f2a1a55e4ee9ae2562758f7
Jayantsinghsaini <146630172+Jayantsinghsaini@users.noreply.github.com>
2025-08-05 09:36:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const port  =  3000
 app.use(cors())
 app.use(express.text())
 app.get('/about',(req,res) => {
-    res.send('<h1>Hello World from backend(server.js)</h1><br><h2>From Backend(server.js)</h2>')
+    res.send('<h1>Hello World from about</h1><br><h2>From Backend(server.js)</h2>')
 })
-
+app.get('/home',(req,res) => {
+    res.send('<h1>Hello World from home</h1><br><h2>From Backend(server.js)</h2>')
+})
 app.listen(port,() => {
     console.log(`app is listening at http://localhost:${port}`);
 })
+
 
 
 
